6787278a4928940b0715a74504296874add6a789
KuroeTaro <kuroetaro@gmail.com>
2026-07-04 01:12:10 +0800

    Update init.lua

--- scenes/load_scene/init.lua
+++ scenes/load_scene/init.lua
@@ -86,13 +86,9 @@ function init_load_scene_anim()
     anim_UI_frame_load_scene_dabo_trig_ease_out_opacity["loop"] = false
 end
 function init_load_scene_image()
-    local image_UI_load_scene_dabo_trig_compressed_image_data = love.image.newCompressedData("asset/load_scene/dabo_trig.dds")
-    local image_UI_load_scene_loading_text_compressed_image_data = love.image.newCompressedData("asset/load_scene/loading_text.dds")
-    local image_UI_load_scene_type_in_mark_compressed_image_data = love.image.newCompressedData("asset/load_scene/type_in_mark.dds")
-
-    image_UI_load_scene_dabo_trig = love.graphics.newImage(image_UI_load_scene_dabo_trig_compressed_image_data)
-    image_UI_load_scene_loading_text = love.graphics.newImage(image_UI_load_scene_loading_text_compressed_image_data)
-    image_UI_load_scene_type_in_mark = love.graphics.newImage(image_UI_load_scene_type_in_mark_compressed_image_data)
+    image_UI_load_scene_dabo_trig = love.graphics.newImage("asset/load_scene/dabo_trig.png")
+    image_UI_load_scene_loading_text = love.graphics.newImage("asset/load_scene/loading_text.png")
+    image_UI_load_scene_type_in_mark = love.graphics.newImage("asset/load_scene/type_in_mark.png")
 end
 function init_load_scene_audio()
     audio_UI_SFX_load_scene_general_end_load = {1}
