7c2f25e2a1682e5bd6d705e6fdc5ae4b330e2256
KuroeTaro <kuroetaro@gmail.com>
2026-07-20 13:22:12 +0800

    --

--- scenes/load_scene/init.lua
+++ scenes/load_scene/init.lua
@@ -87,27 +87,27 @@ function init_load_scene_anim()
     anim_UI_frame_load_scene_dabo_trig_ease_out_opacity["loop"] = false
 end
 function init_load_scene_image()
-    image_UI_load_scene_dabo_trig = love.graphics.newImage("asset/load_scene/dabo_trig.dds")
-    image_UI_load_scene_loading_text = love.graphics.newImage("asset/load_scene/loading_text.dds")
-    image_UI_load_scene_type_in_mark = love.graphics.newImage("asset/load_scene/type_in_mark.dds")
+    image_UI_load_scene_dabo_trig = love.graphics.newImage("asset/load_scene/texture/dabo_trig.dds")
+    image_UI_load_scene_loading_text = love.graphics.newImage("asset/load_scene/texture/loading_text.dds")
+    image_UI_load_scene_type_in_mark = love.graphics.newImage("asset/load_scene/texture/type_in_mark.dds")
 end
 function init_load_scene_audio()
     -- end_load
     audio_UI_SFX_load_scene_general_end_load = {1}
     audio_UI_SFX_load_scene_general_end_load["LCT"] = {0}
     audio_UI_SFX_load_scene_general_end_load["LCD"] = {0}
-    audio_UI_SFX_load_scene_general_end_load["audio"] = love.audio.newSource("asset/load_scene/end_load.wav","static")
+    audio_UI_SFX_load_scene_general_end_load["audio"] = love.audio.newSource("asset/load_scene/audio/end_load.wav","static")
     update_SFX_VOLUME(audio_UI_SFX_load_scene_general_end_load)
     -- loading
     audio_UI_SFX_load_scene_general_loading = {1}
     audio_UI_SFX_load_scene_general_loading["LCT"] = {0}
     audio_UI_SFX_load_scene_general_loading["LCD"] = {0}
-    audio_UI_SFX_load_scene_general_loading["audio"] = love.audio.newSource("asset/load_scene/loading.wav","static")
+    audio_UI_SFX_load_scene_general_loading["audio"] = love.audio.newSource("asset/load_scene/audio/loading.wav","static")
     update_SFX_VOLUME(audio_UI_SFX_load_scene_general_loading)
     -- start_load
     audio_UI_SFX_load_scene_general_start_load = {1}
     audio_UI_SFX_load_scene_general_start_load["LCT"] = {0}
     audio_UI_SFX_load_scene_general_start_load["LCD"] = {0}
-    audio_UI_SFX_load_scene_general_start_load["audio"] = love.audio.newSource("asset/load_scene/start_load.wav","static")
+    audio_UI_SFX_load_scene_general_start_load["audio"] = love.audio.newSource("asset/load_scene/audio/start_load.wav","static")
     update_SFX_VOLUME(audio_UI_SFX_load_scene_general_start_load)
 end
