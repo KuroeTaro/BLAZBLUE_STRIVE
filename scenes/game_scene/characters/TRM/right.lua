499d9f83389655e8f2418075c1a94ac8f113e19f
KuroeTaro <kuroetaro@gmail.com>
2026-08-07 14:59:15 +0800

    --

--- scenes/game_scene/characters/TRM/right.lua
+++ scenes/game_scene/characters/TRM/right.lua
@@ -999,7 +999,9 @@ function load_game_scene_audio_char_RP()
         {folder = "normal/j5K",files = {"j5K_block","j5K_counter","j5K_hit","j5K_whiff"}},
         {folder = "normal/j5S",files = {"j5S_block","j5S_counter","j5S_hit","j5S_whiff"}},
         {folder = "normal/j4_6Launcher",files = {"j4_6Launcher_hit","j4_6Launcher_throw","j4_6Launcher_whiff"}},
-        {folder = "normal/j5Launcher",files = {"j5Launcher_block","j5Launcher_counter","j5Launcher_hit","j5Launcher_whiff"}}
+        {folder = "normal/j5Launcher",files = {"j5Launcher_block","j5Launcher_counter","j5Launcher_hit","j5Launcher_whiff"}},
+        {folder = "special/6SP_P",files = {"6SP_P_ground_bounce","6SP_P_hit","6SP_P_whiff"}},
+        {folder = "special/6SP_S",files = {"6SP_S_block","6SP_S_counter","6SP_S_hit","6SP_S_whiff"}}
     }
     for i = 1,#move_SFX_folder_table do
         local folder = move_SFX_folder_table[i].folder
